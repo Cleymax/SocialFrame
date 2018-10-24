fb9dc323d32192ca85308ead3f626d433d0adf6e
Cleymax (Clément) <clemperrin1@gmail.com>
2018-10-24 20:44:52 +0200

    Initial commit

---
+++ bukkit/src/main/java/fr/cleymax/socialframe/bukkit/SocialFrame.java
@@ -0,0 +1,32 @@
+package fr.cleymax.socialframe.bukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * This file is a part of SocialFrame.
+ * <p>
+ * Copyright (c) SocialFrame <cleymaxpro@gmail.com> - All rights reserved
+ *
+ * @author Clément (Cleymax) {@literal <cleymaxpro@gmail.com>}
+ * Created the 24/10/2018 at 18:40.
+ */
+
+public class SocialFrame extends JavaPlugin {
+
+
+    @Override
+    public void onLoad() {
+
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+
+    }
+}
